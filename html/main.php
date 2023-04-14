c4e62c192bf37760cbfe20ef1b30b827370af6f7
woosungchu <woosungchu@gmail.com>
2023-04-14 14:20:50 +0900

    split header and add caption to carousel

--- html/main.php
+++ html/main.php
@@ -7,9 +7,27 @@
 			<li data-target="#myCarousel" data-slide-to="2"></li>
 		</ol>
 		<div class="carousel-inner">
-			<div class="carousel-item active"><img class="d-block w-100" src="assets/image1.jpg" alt="롤러코스터" title="뒤집어져 떨어지는 롤러코스터"></div>
-			<div class="carousel-item"><img class="d-block w-100" src="assets/image2.jpg" alt="사슴" title="사슴이 풀 먹는 아름다운 자연"></div>
-			<div class="carousel-item"><img class="d-block w-100" src="assets/image3.jpg" alt="호랑이" title="야생 속의 호랑이"></div>
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="assets/image1.jpg" alt="롤러코스터" title="뒤집어져 떨어지는 롤러코스터">
+				<div class="carousel-caption">
+					<h5>뒤집어져 떨어지는 롤러코스터</h5>
+					<p>오금이 저리는...</p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="assets/image2.jpg" alt="사슴" title="사슴이 풀 먹는 아름다운 자연">
+				<div class="carousel-caption">
+					<h5>뒤집어져 떨어지는 롤러코스터</h5>
+					<p>오금이 저리는...</p>
+				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="assets/image3.jpg" alt="호랑이" title="야생 속의 호랑이">
+				<div class="carousel-caption">
+					<h5>뒤집어져 떨어지는 롤러코스터</h5>
+					<p>오금이 저리는...</p>
+				</div>
+				</div>
 		</div>
 	    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
 		  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
--- index.php
+++ index.php
@@ -14,29 +14,7 @@
 
 </head>
 <body>
-<header>
-	<nav class="navbar navbar-expand-md navbar-light">
-		<a class="navbar-brand" href="?page=main">빵집투어<a>
-		<button class="navbar-toggler" type="button"
-		data-toggle="collapse" data-target="navbarCollapse"
-		aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggle-icon"></span>
-		</button>
-		<div id="navbarCollapse" class="collapse navbar-collapse">
-			<ul class="navbar-nav mr-auto"><!-- mr-auto 로 밀어내기 -->
-				<li class="nav-item"><a href="?page=timeline" class="nav-link">빵집소개</a></li>
-				<li class="nav-item"><a href="?page=recommend" class="nav-link">브랜드추천</a></li>
-				<li class="nav-item"><a href="?page=review" class="nav-link">브랜드후기</a></li>
-				<li class="nav-item"><a href="?page=gallery" class="nav-link">갤러리</a></li>
-			</ul>
-			<form class="form-inline mt-md-0 mt-2" style="margin-bottom:0px;">
-				<input class="form-control mr-sm-2" type="text" placeholder="크로와상" aria-label="검색">
-				<button class="btn btn-outline-success" type="submit">검색</button>
-			</form>
-		</div>
-	</nav>
-
-</header>
+<?php include 'html/header.php' ?>
 
 <?php
 switch($_GET['page']) {
