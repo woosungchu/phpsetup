aabaad67ecff4a1aea279a44afe90a00c5c32858
woosungchu <woosungchu@gmail.com>
2023-04-27 11:47:35 +0900

    edit header

--- html/header.php
+++ html/header.php
@@ -2,7 +2,7 @@
 
 <header>
 	<nav class="navbar navbar-expand-md navbar-light">
-		<a class="navbar-brand" href="?page=main">빵집투어<a>
+		<a class="navbar-brand" href="?page=main">제주투어<a>
 		<button class="navbar-toggler" type="button"
 		data-toggle="collapse" data-target="#navbarCollapse"
 		aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,10 +10,10 @@
 		</button>
 		<div id="navbarCollapse" class="collapse navbar-collapse">
 			<ul class="navbar-nav mr-auto">
-				<li class="nav-item"><a href="?page=timeline" class="nav-link">빵집소개</a></li>
-				<li class="nav-item"><a href="?page=recommend" class="nav-link">브랜드추천</a></li>
-				<li class="nav-item"><a href="?page=review" class="nav-link">브랜드후기</a></li>
-				<li class="nav-item"><a href="?page=gallery" class="nav-link">갤러리</a></li>
+				<li class="nav-item"><a href="?page=review" class="nav-link">여행후기</a></li>
+				<li class="nav-item"><a href="?page=recommend" class="nav-link">추천 여행지</a></li>
+				<li class="nav-item"><a href="?page=story" class="nav-link">제주 이야기</a></li>
+				<li class="nav-item"><a href="?page=sites" class="nav-link">사이트 안내</a></li>
 			</ul>
 			<form class="form-inline mt-md-0 mt-2" style="margin-bottom:0px;">
 				<input class="form-control mr-sm-2" type="text" placeholder="크로와상" aria-label="검색">
